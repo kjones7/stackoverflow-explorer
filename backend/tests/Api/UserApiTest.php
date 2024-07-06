8ff55f563dddc4bb908d186037f3889f81bc7464
Kyle Jones <kyleajones242@gmail.com>
2024-07-06 14:53:52 -0400

    Improve User API test by checking more response content

--- backend/tests/Api/UserApiTest.php
+++ backend/tests/Api/UserApiTest.php
@@ -6,12 +6,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserApiTest extends WebTestCase
 {
-    public function testGetProducts(): void
+    public function testGetUserById(): void
     {
         $client = static::createClient();
         $client->request('GET', '/api/users/1');
 
         $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
         $this->assertJson($client->getResponse()->getContent());
+
+        // Assert that the response is the expected JSON
+        $response = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals(1, $response['id']);
+        $this->assertEquals('Jeff Atwood', $response['displayName']);
     }
 }
